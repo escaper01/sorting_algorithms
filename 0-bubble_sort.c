5cdc7b9b2aec8aeebca4be01a18ee18dfe7552b9
escaper <assaoui54@gmail.com>
2023-10-23 23:20:34 +0100

    betty style fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
@@ -17,6 +18,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				int tmp = array[j + 1];
+
 				array[j + 1] = array[j];
 				array[j] = tmp;
 				print_array(array, size);
