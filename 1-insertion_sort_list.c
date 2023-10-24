001d8192751b210fef5809e8720671f607794eb4
escaper <assaoui54@gmail.com>
2023-10-24 21:00:29 +0100

    emmmmmmmm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,12 +9,12 @@
 void insertion_sort_list(listint_t **list)
 {
 
-	listint_t *current = (*list)->next;
-
 	if (list == NULL)
 	{
 		return;
 	}
+
+	listint_t *current = (*list)->next;
 
 	while (current != NULL)
 	{
