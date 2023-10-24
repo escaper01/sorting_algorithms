ceb16ef5e4b1bba5c1a638a9d5fb7148c7acfbd5
escaper <assaoui54@gmail.com>
2023-10-24 20:38:48 +0100

    done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,4 +6,37 @@
  * insertion_sort_list - insertion sort list
  * @list: list to be sorted
  */
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *current = (*list)->next;
+
+	while (current != NULL)
+	{
+		listint_t *insertionPoint = current->prev;
+
+		while (insertionPoint != NULL && current->n < insertionPoint->n)
+		{
+
+			if (current->next != NULL)
+			{
+				current->next->prev = insertionPoint;
+			}
+			if (insertionPoint->prev != NULL)
+			{
+				insertionPoint->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			current->prev = insertionPoint->prev;
+			insertionPoint->next = current->next;
+			current->next = insertionPoint;
+			insertionPoint->prev = current;
+			insertionPoint = current->prev;
+		}
+		print_list(*list);
+		current = current->next;
+	}
+}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,5 +6,32 @@
  * selection_sort - selection sort an array
  * @array: arr to sort
  * @size: arr size
-*/
-void selection_sort(int *array, size_t size);
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned long int j = 0;
+	size_t i;
+	int tmp;
+
+	while (j < size - 1)
+	{
+		int last_min = array[j];
+		int last_index = j;
+
+		for (i = j + 1; i < size; i++)
+		{
+			if (array[i] < last_min)
+			{
+				last_min = array[i];
+				last_index = i;
+			}
+		}
+
+		tmp = array[j];
+		array[j] = array[last_index];
+		array[last_index] = tmp;
+
+		j++;
+		print_array(array, size);
+	}
+}
